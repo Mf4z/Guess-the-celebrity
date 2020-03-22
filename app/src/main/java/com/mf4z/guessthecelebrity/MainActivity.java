db38ba0bcffce6ff289a6452c4f5abb64c048049
Marwan <emef4z@gmail.com>
2020-03-22 17:19:31 +0400

    Complete game features

--- app/src/main/java/com/mf4z/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/mf4z/guessthecelebrity/MainActivity.java
@@ -7,34 +7,46 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] celeb_url;
-    String[] celeb_name;
+    ArrayList<String> celeb_url;
+    ArrayList<String> celeb_name;
 
-private static final String site_url = "http://www.posh24.se/kandisar";
+    private static final String site_url = "http://www.posh24.se/kandisar";
     ImageView imageView;
     Button button0;
     Button button1;
     Button button2;
     Button button3;
+    private Random mRandom;
+    private int mChosenCeleb;
+    private int mLocationOfCorrect;
+    private ArrayList<String> mAnswers;
+    private Pattern mPattern;
+    private Matcher mMatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        celeb_url = new ArrayList<String>();
+        celeb_name = new ArrayList<String>();
+        mAnswers = new ArrayList<>();
 
         imageView = (ImageView) findViewById(R.id.imageView);
         button0 = (Button) findViewById(R.id.button_answer_0);
@@ -44,32 +56,46 @@ private static final String site_url = "http://www.posh24.se/kandisar";
 
         DownloadCelebData celebData = new DownloadCelebData();
 
+        //Holds result of Dowloaded celeb info from the web in html
         String result = null;
+
         try {
             result = celebData.execute(site_url).get();
+
+            String[] splitString = result.split("<div class=\"listedArticles\">");
+
+            //Finds celeb photo url from result and adds it to the ArrayList of celeb photo urls
+            findPhotoUrlFromResult(splitString[0]);
+
+            Log.i("Celebrity", splitString[0]);
+
+            //Finds celeb name from result and adds it to the ArrayList of celeb names
+            findCelebNameFromResult(splitString[0]);
+
         } catch (Exception e) {
 
             e.printStackTrace();
         }
 
-        //Using Regex to get Photo url
-        Pattern p = Pattern.compile("<img src=\"(.*?)\"");
-        Matcher m = p.matcher(result);
-
-        int count = 0;
-        while(m.find()){
-            //celeb_url[count] = m.group(1);
-            Log.i("Celebrity_url_photos",m.group(1));
 
 
-            count++;
+
+
+        for (String url : celeb_url) {
+
+            Log.i("Celebrity_url_photos", url);
+
         }
 
-        Log.i("Celebrity",result);
 
-//        count = 0;
-//        while (count < celeb_url.length)
-//        Log.i("Celebrity_url_photos",celeb_url[count]);
+        for (String url : celeb_name) {
+
+            Log.i("Celebrity_url_names", url);
+
+        }
+
+
+        setGame();
     }
 
 
@@ -125,6 +151,108 @@ private static final String site_url = "http://www.posh24.se/kandisar";
                 return "Failed" + e;
             }
         }
+    }
+
+
+    private void setGame() {
+
+        mRandom = new Random();
+        mChosenCeleb = mRandom.nextInt(celeb_url.size());
+        DownloadTopCelebsPhoto photo = new DownloadTopCelebsPhoto();
+
+        String photo_url = celeb_url.get(mChosenCeleb);
+
+        Bitmap celeb_photo;
+
+        try {
+            celeb_photo = photo.execute(photo_url).get();
+            imageView.setImageBitmap(celeb_photo);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        mLocationOfCorrect = mRandom.nextInt(4);
+
+        int incorrectAnswerLocation = 0;
+
+        mAnswers.clear();
+
+        for (int i= 0; i < 4; i++){
+
+                if (i == mLocationOfCorrect){
+                    mAnswers.add(celeb_name.get(mChosenCeleb));
+                }
+
+                else{
+
+                    incorrectAnswerLocation = mRandom.nextInt(celeb_url.size());
+
+                    while (incorrectAnswerLocation == mChosenCeleb){
+
+                        incorrectAnswerLocation = mRandom.nextInt(celeb_url.size());
+                    }
+
+                    mAnswers.add(celeb_name.get(incorrectAnswerLocation));
+                }
+        }
+
+        button0.setText(mAnswers.get(0));
+        button1.setText(mAnswers.get(1));
+        button2.setText(mAnswers.get(2));
+        button3.setText(mAnswers.get(3));
+
+
+
+
+    }
+
+    private void findPhotoUrlFromResult(String result) {
+
+        //Using Regex to get Photo url
+        mPattern = Pattern.compile("<img src=\"(.*?)\"");
+        mMatcher = mPattern.matcher(result);
+
+        while (mMatcher.find()) {
+            String found = mMatcher.group(1);
+            System.out.println(found);
+            celeb_url.add(found);
+        }
+
+    }
+
+
+    private void findCelebNameFromResult(String result) {
+
+        //Using Regex to get Celeb name
+        mPattern = Pattern.compile("alt=\"(.*?)\"");
+        mMatcher = mPattern.matcher(result);
+
+        while (mMatcher.find()) {
+            String found = mMatcher.group(1);
+            System.out.println(found);
+            celeb_name.add(found);
+        }
+
+    }
+
+    public void chosenCeleb(View view){
+
+        String selectedTag =  view.getTag().toString();
+        int tag = Integer.parseInt(selectedTag);
+        int correctAnswer = mLocationOfCorrect;
+
+        if(tag == correctAnswer){
+            Toast.makeText(this,"Correct!",Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Toast.makeText(this,"wrong! It was : "+mAnswers.get(correctAnswer),Toast.LENGTH_SHORT).show();
+        }
+
+        //Resets game after correct guess
+        setGame();
+
     }
 }
 
